7b7b6e0d602e5d6182d96c98afc96b6e9dce1573
szymonnedzi <szymon.nedzi@gmail.com>
2023-08-16 12:15:48 +0200

    small changes

--- src/main/java/simon/dragonfly/controllers/DragonFlyController.java
+++ src/main/java/simon/dragonfly/controllers/DragonFlyController.java
@@ -1,5 +1,7 @@
 package simon.dragonfly.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//Why does this documentation not work nice?
+@Api(tags = "Dragonfly Controller", description = "APIs for checking the status of Dragonfly application")
 public class DragonFlyController {
 
     private static final Logger logger = LoggerFactory.getLogger(DragonFlyController.class);
-    
+
     @GetMapping("/")
+    @ApiOperation(value = "Check the status of the Dragonfly application",
+                  notes = "This endpoint allows you to check if the Dragonfly application is online.",
+                  response = ResponseEntity.class)
     public ResponseEntity<String> checkAppStatus() {
         try {
             logger.info("Checking Dragonfly status...");
